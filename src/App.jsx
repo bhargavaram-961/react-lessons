579cecc1da68a050aff679461a9055143a746f2c
bhargavaram-961 <bhargavaram961@gmail.com>
2025-05-22 20:36:33 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import App1 from "./Components/App1";
 import App2 from "./Components/App2";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Test from "./Components/Test";
 function App() {
   return (
     <BrowserRouter>
       <header>
         <h1>React Lessons</h1>
         <Link to="/">App1</Link>-
-        <Link to="/app2">App2</Link>
+        <Link to="/app2">App2</Link>-
+        <Link to="/test">Test</Link>
       </header>
       <hr />
       <main>
@@ -19,6 +21,7 @@ function App() {
           <Route index element={<App1/>}></Route>
           <Route path="/" element={<App1/>}></Route>
           <Route path="/app2" element={<App2/>}></Route>
+          <Route path="/test" element={<Test/>}></Route>
         </Routes>
       </main>
       <hr />
