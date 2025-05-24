1dba35a38833bacd9e870892d6f68ecc0501331a
bhargavaram-961 <bhargavaram961@gmail.com>
2025-05-24 19:51:45 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
+
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import App1 from "./Components/App1";
-import App2 from "./Components/App2";
+import App1 from "./components/App1";
+import App2 from "./components/App2";
+import App3 from "./components/App3";
+import App4 from "./components/App4";
+import App5 from "./components/App5";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Test from "./Components/Test";
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <h1>React Lessons</h1>
-        <Link to="/">App1</Link>-
-        <Link to="/app2">App2</Link>-
-        <Link to="/test">Test</Link>
-      </header>
-      <hr />
-      <main>
-        <Routes>
-          <Route index element={<App1/>}></Route>
-          <Route path="/" element={<App1/>}></Route>
-          <Route path="/app2" element={<App2/>}></Route>
-          <Route path="/test" element={<Test/>}></Route>
-        </Routes>
-      </main>
-      <hr />
-      <footer>Copyright. All rights reserved.</footer>
+      <Header />
+      <Routes>
+        <Route index element={<App1 />}></Route>
+        <Route path="/" element={<App1 />}></Route>
+        <Route path="/app2" element={<App2 />}></Route>
+        <Route path="/app3" element={<App3 />}></Route>
+        <Route path="/app4" element={<App4 />}></Route>
+         <Route path="/app5" element={<App5 />}></Route>
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,13 +1,3 @@
-import App1 from "./App1";
-import App2 from "./App2";
-import Test from "./Test";
-
 export default function Product() {
-    return (
-      <>
-        <div>this is product component</div>
-        <Test />
-      </>
-    );
-
+  return <h1>This is Product</h1>;
 }
